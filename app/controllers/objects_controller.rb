15f5a8e67fc51f8ae06ca65ceac1bb7d1fec75d5
AwsKiel <rkiel@consultwithcase.com>
2014-01-16 11:23:25 -0500

    added hard coded object upload

--- app/controllers/objects_controller.rb
+++ app/controllers/objects_controller.rb
@@ -3,9 +3,19 @@ class ObjectsController < ApplicationController
   before_action :lookup
 
   def index
+    FileUtils.chdir "/vagrant/app/assets/images" do
+      Dir.glob("*.jpg").each do |name|
+        @bucket.objects.create(name,File.read(name), :acl => :public_read)
+      end
+    end
     @objects = @bucket.objects
   end
 
+  def destroy
+    bucket = @s3.buckets[params[:bucket_id]]
+    bucket.objects[params[:id]].delete
+    redirect_to action: :index
+  end
 private
 
   def lookup
